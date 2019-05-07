745a9f4bcb8360c2695578ed7e6862566f8032e7
Emilia Ziemichód <emifrompoland@gmail.com>
2019-05-07 05:46:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,4 +2,14 @@ require_relative "../lib/user.rb
 
 class Student<User
 
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge=[]
+  end
+
+  def learn(knowledge)
+    @knowledge<<knowledge
+  end
+
 end
